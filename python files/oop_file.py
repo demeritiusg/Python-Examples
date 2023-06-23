58f8a3461aa36bcf33123e37fc1f32a94eea9cbf
demeritiusg <demeritiusgriffin@gmail.com>
2023-06-22 19:13:14 -0700

    adding some changes

---
+++ python files/oop_file.py
@@ -0,0 +1,25 @@
+
+class Person():
+    multiplier = 69
+    def __init__(self, name, number):
+        if not isinstance(name, str) or not isinstance(number, int):
+            raise ValueError('wrong values')
+        self.name = name
+        self.number = number
+
+    def display(self):
+        print(self.name)
+        print(self.number)
+
+    def details(self):
+        print(f'the persons name is {self.name} and number is {self.number}')
+
+    def some_math(self):
+        x = self.multiplier*self.number
+        return x
+
+    
+p = Person('Demeritius', 45)
+
+p.details()
+print(p.some_math())
