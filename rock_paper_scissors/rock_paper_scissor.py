9257a6399496c3e08bbfee1c8ec478302bc79a18
demeritiusg <demeritiusgriffin@gmail.com>
2023-06-26 13:40:12 -0700

    updated code

--- rock_paper_scissors/rock_paper_scissor.py
+++ rock_paper_scissors/rock_paper_scissor.py
@@ -6,13 +6,13 @@ class Action(IntEnum):
     Paper = 1
     Scissors = 2
 
-def user_selection():
+def user_pick():
     user_input = input("enter a choice (rock[0], paper[1], scissors[2]):")
     selection = int(user_input)
     action = Action(selection)
     return action
 
-def computer_selection():
+def computer_pick():
     selection = random.randint(0, len(Action) - 1)
     action = Action(selection)
     return action
@@ -38,8 +38,19 @@ def play_game(user_selection, computer_selection):
         else:
             print('Rock smashes scissors. you lose.')
     
-    play_again = input('Play again? (y/n): ')
+    
+while True:
+    try:
+        user_selection = user_pick()
+    except ValueError as e:
+        select_range = f'[0, {len(Action) - 1}]'    
+        print(f'range out of bounds, 0 - {select_range}')
+        continue
 
+    computer_selection = computer_pick()
+    play_again(user_selection, computer_selection)
+
+    play_again = input('Play again? (y/n): ')
     if play_again.lower() != 'y':
         break
     
