7566f74d323ee5883ae9f15bc1e7b7e2d0490c9d
demeritiusg <demeritiusgriffin@gmail.com>
2024-01-16 17:02:58 -0800

    new files

---
+++ rock_paper_scissors/rock_paper_scissors_2.py
@@ -0,0 +1,55 @@
+import random
+from enum import IntEnum
+
+class Action(IntEnum):
+    Rock = 1
+    Paper = 2
+    Scissors = 3
+
+def user_pick():
+    choices = [f'{action.name}[{action.value}]' for action in Action]
+    choices_str = ', '.join(choices)
+    selection = int(input(f'Enter a choice {choices_str}):'))
+    action = Action(selection)
+    return action
+
+def computer_pick():
+    selection = random.randint(0, len(Action) - 1)
+    action = Action(selection)
+    return action
+
+def play_game(user_selection, computer_selection):
+
+    if user_selection == computer_selection:
+        print(f'both players selected {user_selection.name}')
+    elif user_selection == Action.Rock:
+        if computer_selection == Action.Scissors:
+            print('rock smashes scissors! You win!')
+        else:
+            print('Paper covers rock! You lose.')
+    elif user_selection == Action.Paper:
+        if computer_selection == Action.Rock:
+            print('Paper covers rock!, you win!')
+        else:
+            print('Scissors cuts paper! you lose.')
+    elif user_selection == Action.Scissors:
+        if computer_selection == Action.Paper:
+            print('Scissors cuts paper! you win!')
+        else:
+            print('Rock smashes scissors. you lose.')
+    
+while True:
+    try:
+        user_selection = user_pick()
+    except ValueError as e:
+        select_range = f'[0, {len(Action) - 1}]'    
+        print(f'range out of bounds, 0 - {select_range}')
+        continue
+
+    computer_selection = computer_pick()
+    play_again(user_selection, computer_selection)
+
+    play_again = input('Play again? (y/n): ')
+    if play_again.lower() != 'y':
+        break
+    
