197f6060ac71891e60a0b7a0c2aa500d6bbca7ca
demeritiusg <demeritiusgriffin@gmail.com>
2026-07-03 19:17:24 -0700

    added new updated glue file and updated glue file

--- ETL/scripts/glue_transform_data.py
+++ ETL/scripts/glue_transform_data.py
@@ -1,64 +1,83 @@
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
+
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
+from awsglue.transforms import ApplyMapping
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from pyspark.sql.functions import col
 
-args = getResolvedOptions(sys.argv, ['glue_job', 'obj_key'])
-obj_key = args['obj_key']
+args = getResolvedOptions(
+    sys.argv,
+    [
+        'glue_job',
+        'obj_key',
+        'source_bucket',
+        'destination_bucket',
+        'database_name',
+        'table_name',
+    ],
+)
 
-# initializing context and job
+obj_key = args.get('obj_key', '')
+source_bucket = args.get('source_bucket', '')
+destination_bucket = args.get('destination_bucket', '')
+database_name = args.get('database_name', 'events')
+table_name = args.get('table_name', 'divisions')
+
 sc = SparkContext()
 gc = GlueContext(sc)
-spark = sc.spark_session
 job = Job(gc)
 job.init(args['glue_job'], args)
 
-# define source and destination buckets
-source_bucket = ''
-destination_bucket = ''
-source_path = f's3://{source_bucket}/{obj_key}'
-output_path = 's3://' + destination_bucket + '/' + obj_key.rsplit(".",1)[0]
+try:
+    read_data = gc.create_dynamic_frame.from_catalog(
+        database=database_name,
+        table_name=table_name,
+        transformation_ctx='read_data',
+    )
 
-# read data from catlog... assumed data has been crawled
-read_data = gc.create_dynamic_frame.from_catlog(
-	database='events',
-	table_name='divisions'
-	)
+    cleaned_df = read_data.toDF().where(col('event_category_id').isNotNull())
+    mapped_frame = DynamicFrame.fromDF(cleaned_df, gc, 'mapped_frame')
 
-remove_nulls = read_data.toDF()
-remove_nulls = remove_nulls.where("'event_category_id' is NOT NULL")
+    transform_data = ApplyMapping.apply(
+        frame=mapped_frame,
+        mappings=[
+            ('date', 'date', 'date', 'date'),
+            ('event_category_id', 'long', 'event_category_id', 'long'),
+            ('event_division', 'string', 'event_division', 'string'),
+            ('event_start_date', 'date', 'event_start_date', 'date'),
+            ('visit_days', 'long', 'visit_days', 'long'),
+            ('DivVisitors', 'long', 'DivVisitors', 'long'),
+            ('DivCustomers', 'long', 'DivCustomers', 'long'),
+            ('newstylecount', 'int', 'newstylecount', 'int'),
+            ('reusedstylecount', 'int', 'reusedstylecount', 'int'),
+            ('totalstyles', 'int', 'totalstyles', 'int'),
+            ('grossdemand', 'float', 'grossdemand', 'float'),
+            ('totalskus', 'int', 'totalskus', 'int'),
+            ('vendors', 'int', 'vendors', 'int'),
+            ('startinginvunits', 'double', 'startinginvunits', 'double'),
+            ('Customers', 'int', 'Customers', 'int'),
+            ('COGS', 'int', 'COGS', 'int'),
+            ('UnitQty', 'int', 'UnitQty', 'int'),
+        ],
+        transformation_ctx='transform_data',
+    )
 
-tmp_df = DynamicFrame.fromDF(remove_nulls, gc)
-transform_data = tmp_df.applyMapping.apply([
-    ('date', 'date'),
-    ('event_category_id', 'long'),
-    ('event_division', 'string'),
-    ('event_start_date', 'date'),
-    ('visit_days', 'long'),
-    ('DivVisitors', 'long'),
-    ('DivCustomers', 'long'),
-    ('newstylecount', 'int'),
-    ('reusedstylecount', 'int'),
-    ('totalstyles', 'int'),
-    ('grossdemand', 'float'),
-    ('totalskus', 'int'),
-    ('vendors', 'int'),
-    ('startinginvunits', 'double'),
-    ('Customers', 'int'),
-    ('COGS', 'int'),
-    ('UnitQty', 'int')]
-	)
+    if not destination_bucket:
+        raise ValueError('destination_bucket argument is required')
 
-#write the data to s3
-gc.write_dynamic_frame.from_options(
-	frame = transform_data,
-	connection_type = "s3",
-	connection_options = {"path": output_path},
-	format = 'parquet'
-	)
+    output_path = f's3://{destination_bucket}/{obj_key.rsplit(".", 1)[0]}' if obj_key else f's3://{destination_bucket}/output'
 
-# finish job
-job.commit()
+    gc.write_dynamic_frame.from_options(
+        frame=transform_data,
+        connection_type='s3',
+        connection_options={'path': output_path},
+        format='parquet',
+    )
+
+    job.commit()
+except Exception:
+    job.commit()
+    raise
