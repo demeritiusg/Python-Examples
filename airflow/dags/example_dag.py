50f3a7ca9af8e999e3a0f4de602506e70082ba33
demeritiusg <demeritiusgriffin@gmail.com>
2026-07-08 15:24:26 -0700

    update examples

--- airflow/dags/example_dag.py
+++ airflow/dags/example_dag.py
@@ -26,17 +26,19 @@ with DAG(
         return "Begin processing"
 
     @task
-    def fetch_user_data(user):
-        # Simulate possible failure
-        # if random.random() < 0.1:
+    def fetch_user(user):
+        # Simulate fetching user name from API 
+        user = user.capitalize()
         #     raise ValueError(f"Failed to fetch data for {user}")
-        return {"user": user, "score": random.randint(0, 100)}
+        return {"user": user}
 
     @task.branch
-    def classify_user(score):
-            if score >= 50:
-                return "high_score"
-            return "low score"
+    def classify_user():
+        score = random.randint(0, 100)
+        
+        if score >= 50:
+            return "high_score"
+        return "low score"
 
     # def branch_func(classification: str):
     #     return f"{classification}"
@@ -53,22 +55,21 @@ with DAG(
     def cleanup(user):
         print(f"Cleanup done for {user}")
 
-    start_node = start()
-
     for user in users:
-        user_data = fetch_user_data.override(task_id=f"fetch_{user}")(user)
-        user_class = classify_user.override(task_id=f"classify_{user}")(user_data)
+        user_data = fetch_user.override(task_id=f"fetch_{user}")(user)
+        #user_class = classify_user.override(task_id=f"classify_{user}")(user_data)
         branch = BranchPythonOperator(
             task_id=f"branch_{user}",
             python_callable=classify_user,
             op_args=[user_class],
         )
 
-        hs = high_score_process.override(task_id=f"high_score_{user}")(user_data)
-        ls = low_score_process.override(task_id=f"low_score_{user}")(user_data)
+        hs = high_score.override(task_id=f"high_score_{user}")(user_data)
+        ls = low_score.override(task_id=f"low_score_{user}")(user_data)
 
         end = cleanup.override(task_id=f"cleanup_{user}")(user)
 
-        start_node >> user_data >> user_class >> branch
+        user_data >> user_class >> branch
+        
         branch >> hs >> end
         branch >> ls >> end
